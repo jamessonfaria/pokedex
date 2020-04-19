354710cabe1963b44632a37231b3c32e6027934d
Jamesson Faria <jamessonjr@gmail.com>
2020-04-18 22:53:06 -0300

    add animation in project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppComponent } from './app.component';
 import { RollOnScrollDirective } from './directives/roll-on-scroll.directive';
@@ -16,7 +17,8 @@ import { RollOnScrollDirective } from './directives/roll-on-scroll.directive';
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
